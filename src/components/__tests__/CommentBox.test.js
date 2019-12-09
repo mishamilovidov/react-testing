22759c8841aef5a7fdb8b4b793a9431aa0ee8615
Misha Milovidov <mishamilovidov@icloud.com>
2019-12-08 16:17:43 -0800

    Add test to comment box component

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -24,4 +24,14 @@ it('has a text area that users can type in', () => {
   wrapped.update();
 
   expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-})
+});
+
+it('has emptied text area when form is submitted', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  wrapped.update();
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
